895cd2e2d956c61eb58ac750ef6533a4e1316e3f
DavidHruby1 <hrubyd74@gmail.com>
2025-12-15 14:39:06 +0100

    Add App layout and update .gitignore

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,18 @@
+import { TodoHeader } from '../modules/header/TodoHeader.tsx';
+import { TodoInput } from '../modules/todo-input/TodoInput.tsx';
+import { TodoList } from '../modules/todo-list/TodoList.tsx';
+import styles from './App.module.css';
+
 
 function App() {
     return (
-        <></>
+        <>
+            <div className={styles["app-container"]}>
+                <TodoHeader />
+                <TodoInput />
+                <TodoList />
+            </div>
+        </>
     );
 }
 
