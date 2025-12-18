ea4ac0cccbb064e0e8c167cba66699fb2dca5314
DavidHruby1 <hrubyd74@gmail.com>
2025-12-18 09:26:51 +0100

    Integrate TodoContext into the App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,19 @@
-import { TodoHeader } from '../modules/header/TodoHeader.tsx';
-import { TodoInput } from '../modules/todo-input/TodoInput.tsx';
-import { TodoList } from '../modules/todo-list/TodoList.tsx';
+import { TodoProvider } from 'src/modules/common/context/todo/TodoContext.tsx';
+import { TodoHeader } from 'src/modules/header/TodoHeader.tsx';
+import { TodoInput } from 'src/modules/todo-input/TodoInput.tsx';
+import { TodoList } from 'src/modules/todo-list/TodoList.tsx';
 import styles from './App.module.css';
 
 
 function App() {
     return (
-        <>
+        <TodoProvider>
             <div className={styles["app-container"]}>
                 <TodoHeader />
                 <TodoInput />
                 <TodoList />
             </div>
-        </>
+        </TodoProvider>
     );
 }
 
--- src/modules/common/context/todo/useTodo.ts
+++ src/modules/common/context/todo/useTodo.ts
@@ -5,7 +5,7 @@ import type { TodoContextType } from './TodoContext';
 export const useTodo = (): TodoContextType => {
     const context = useContext(TodoContext);
 
-    if (!context) {
+    if (!context) { // This will show Toast if error, in the future
         throw new Error("useTodo must be used within TodoProvider");
     }
 
