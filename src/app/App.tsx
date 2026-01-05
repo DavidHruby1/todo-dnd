007fa4a19ee5d9a07e4001b3991ae67047fd8146
DavidHruby1 <hrubyd74@gmail.com>
2026-01-05 17:37:35 +0100

    Wrap the App component in ToastProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,19 @@ import { TodoHeader } from 'src/modules/header/TodoHeader.tsx';
 import { TodoInput } from 'src/modules/todo-input/TodoInput.tsx';
 import { TodoList } from 'src/modules/todo-list/TodoList.tsx';
 import styles from './App.module.css';
+import { ToastProvider } from 'src/modules/common/context/toast/ToastContext';
 
 
 function App() {
     return (
         <TodoProvider>
-            <div className={styles["app-container"]}>
-                <TodoHeader />
-                <TodoInput />
-                <TodoList />
-            </div>
+            <ToastProvider>
+                <div className={styles["app-container"]}>
+                    <TodoHeader />
+                    <TodoInput />
+                    <TodoList />
+                </div>
+            </ToastProvider>
         </TodoProvider>
     );
 }
